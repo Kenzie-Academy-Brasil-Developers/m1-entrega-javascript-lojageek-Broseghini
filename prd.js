358420b4dc532ab9fa2d4b07bbb926be2f4ef4aa
Matheus Broseghini <mateus_guarnier@hotmail.com>
2022-07-11 18:46:49 -0300

    Atividade Refeita

---
+++ prd.js
@@ -0,0 +1,38 @@
+const produtos = [
+    {
+        nome:"Anime - Zero Two",
+        preco:500,
+        departamento: "Actions",
+        imgeUrl:"animewoman.jpg"
+    },
+    {
+        nome:"Dragon Ball",
+        preco:600,
+        departamento: "Actions",
+        imgeUrl:"dragonballpersonagem.jpg"
+    },
+    {
+        nome:"Bebê Yoda",
+        preco:1000,
+        departamento: "Actions",
+        imgeUrl:"starwarspersonagem.jpg"
+    },
+    {
+        nome:"Clock",
+        preco:150,
+        departamento: "Frames",
+        imgeUrl:"clock.jpg"
+    },
+    {
+        nome:"Controle",
+        preco:200,
+        departamento:"Frames",
+        imgeUrl:"gamepad.jpg"
+    },
+    {
+        nome:"C-3PO",
+        preco:350,
+        departamento:"Frames",
+        imgeUrl:"personagem.jpg"
+    }
+]
--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-let item = {
-        ""
+let vitrineActions   = document.querySelector(".Actions")
+let vitrineFrames        = document.querySelector(".Frames")
 
 
+
+function listarProdutos(listaProdutos, departamentoVitrine, vitrine){
+
+    
+    for(let i = 0; i<listaProdutos.length; i++){
+        
+        let produto             = listaProdutos[i]
+        let departamentoProduto = produto.departamento
+        
+        
+        if(departamentoProduto == departamentoVitrine){
+            let cardProduto    = criarCardProduto(produto)
+            vitrine.appendChild(cardProduto)
+        }
+    }
+   
 }
+listarProdutos(produtos, "Actions", vitrineActions)
+listarProdutos(produtos, "Frames", vitrineFrames)
 
-// function createActionItem(objeto){
-//     let item = {
 
-//     }
+function criarCardProduto(produto){
 
-// };
+     
+    let nome            = produto.nome
+    let preco           = produto.preco
+    let imgeUrl         = produto.imgeUrl
+  
+    
+    let tagLi       = document.createElement("li")
+    let tagImge     = document.createElement("img")
+    let tagNome     = document.createElement("h2")
+    let tagPreco    = document.createElement("p")
+    let btnComprar  = document.createElement("button")
+    
+    tagImge.classList.add("imagemProduto")
 
-// function createPaintingItem(objeto){
-//     let listFigure = [];
-//     let listPainting = [];
-// }
+     
+    tagImge.src          = `/img/${imgeUrl}`
+    tagImge.alt          = nome
+    tagNome.innerText    = nome
+    tagPreco.innerText   = `R$ ${preco}`
+  
+    
+    tagLi.appendChild(tagImge)
+    tagLi.appendChild(tagNome)
+    tagLi.appendChild(tagPreco)
+
+    return tagLi
+}
 
